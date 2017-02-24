69ce91d7faba89cf83e2c0bbf1f64242030b4241
LilJunJun <goninar@gmail.com>
2017-02-24 18:00:18 -0500

    Changes from 209

---
+++ TheLostChild-development/TheLostChild/Assets/_Scripts/NR/FlashLight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLight : MonoBehaviour 
+{
+	public Light light;
+	public AudioClip on;
+	public AudioClip off;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.LeftShift))//user "input" in this case Shift key to turn
+												//flashlight on or off
+		{
+			if (light.enabled == false)
+			{
+				light.enabled = true;//turns flashlight on
+				GetComponent<AudioSource>().clip = on;
+				GetComponent<AudioSource>().Play();
+			}
+						
+			else
+			{
+				light.enabled = false;//turns flashlight off
+				GetComponent<AudioSource>().clip = off;
+				GetComponent<AudioSource>().Play();
+			}
+		
+		}
+	}
+}
