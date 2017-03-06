acb0f5ca985da0a618c158c2857d08572c33f1e2
LilJunJun <goninar@gmail.com>
2017-03-06 00:59:43 -0500

    Jump Scares & Audio WIP
    Added in some jump scares on the second floor and scripts for triggering
    things under folder NR2.  These scripts can be used for anything :)

---
+++ TheLostChild-development/TheLostChild/Assets/_Scripts/NR/NR2/audioTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioTrigger : MonoBehaviour 
+{
+	AudioSource audio;
+	bool alreadyPlayed = false;
+	public AudioClip myAudio;
+
+	void Start()
+	{
+		audio = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter(Collider collision) 
+	{
+		if (collision.gameObject.tag == "Ivanoffee") 
+		{
+			if (!alreadyPlayed)
+			{
+				audio.PlayOneShot (myAudio, 1);// 0-1 for volume
+				alreadyPlayed = true; //make sure it only plays once
+			}
+		}
+	}
+	/*
+	AudioSource audio = GetComponent<AudioSource>();
+
+	void OnTriggerEnter(Collider collision) 
+	{
+		if (collision.gameObject.tag == "Ivanoffee") 
+		{
+			if (!audio.isPlaying)
+			{
+				audio.Play ();
+			}
+		}
+	}
+	*/
+}
---
+++ TheLostChild-development/TheLostChild/Assets/_Scripts/NR/NR2/setActive.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setActive : MonoBehaviour 
+{
+	public GameObject myObject; 
+	public GameObject myObject2; 
+
+	void OnTriggerEnter(Collider collision) 
+	{
+		if (collision.gameObject.tag == "Ivanoffee") 
+		{
+			myObject.SetActive (true);
+			myObject2.SetActive (true);
+		}
+	}
+
+}
